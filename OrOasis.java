8c84759ba8244a37ae7e3ee5f1e239632d5b4f7d
Ritikka004 <ritikkajaganathan@gmail.com>
2026-03-17 19:38:58 +0530

    Add Java solution for OrOasis problem
    This problem finds the minimum length subarray(s) such that the bitwise OR of the chosen subarray equals the OR of the remaining elements. The solution uses prefix and suffix OR arrays to efficiently compute the OR of elements outside the current subarray. By checking all valid subarrays, it determines the smallest length and counts how many such subarrays exist.

---
+++ OrOasis.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            String[] parts = br.readLine().split(" ");
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+                arr[i] = Integer.parseInt(parts[i]);
+            int[] prefix = new int[n];
+            int[] suffix = new int[n];
+            prefix[0] = arr[0];
+            for(int i=1;i<n;i++)
+                prefix[i] = prefix[i-1] | arr[i];
+            suffix[n-1] = arr[n-1];
+            for(int i=n-2;i>=0;i--)
+                suffix[i] = suffix[i+1] | arr[i];
+            int minLen = Integer.MAX_VALUE;
+            int count = 0;
+
+            for(int i=0;i<n;i++){
+                int insideOR = 0;
+                for(int j=i;j<n;j++){
+                    int len = j-i+1;
+                    if(len > minLen)
+                        break;
+                    insideOR |= arr[j];
+                    int outsideOR = 0;
+                    if(i>0)
+                        outsideOR |= prefix[i-1];
+                    if(j<n-1)
+                        outsideOR |= suffix[j+1];
+                    if(insideOR == outsideOR){
+                        if(len < minLen){
+                            minLen = len;
+                            count = 1;
+                        }
+                        else if(len == minLen){
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            if(count==0)
+                System.out.println(-1);
+            else
+                System.out.println(minLen + " " + count);
+        }
+    }
+}
